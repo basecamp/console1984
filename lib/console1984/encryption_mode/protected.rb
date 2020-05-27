0345fc72c1f65858d03d963925517f30ba3cf2f5
Jorge Manrubia <jorge.manrubia@gmail.com>
2020-05-27 18:43:31 +0200

    Update active_record_encryption and use the new frozen encryption for protected mode

--- lib/console1984/encryption_mode/protected.rb
+++ lib/console1984/encryption_mode/protected.rb
@@ -1,6 +1,6 @@
 class Console1984::EncryptionMode::Protected
   def execute(&block)
-    ActiveRecordEncryption.with_encryption_context(encryptor: read_only_null_encryptor, &block)
+    ActiveRecordEncryption.with_encryption_context(encryptor: read_only_null_encryptor, frozen_encryption: true, &block)
   end
 
   private
