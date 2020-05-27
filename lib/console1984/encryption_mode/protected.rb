7443a993a4d08864b06a3b2bdc063d3b3eb21595
Jorge Manrubia <jorge.manrubia@gmail.com>
2020-05-27 19:18:20 +0200

    We should use a regular null encrptor now
    The frozen option is the protection now

--- lib/console1984/encryption_mode/protected.rb
+++ lib/console1984/encryption_mode/protected.rb
@@ -1,10 +1,10 @@
 class Console1984::EncryptionMode::Protected
   def execute(&block)
-    ActiveRecordEncryption.with_encryption_context(encryptor: read_only_null_encryptor, frozen_encryption: true, &block)
+    ActiveRecordEncryption.with_encryption_context(encryptor: null_encryptor, frozen_encryption: true, &block)
   end
 
   private
-    def read_only_null_encryptor
-      @read_only_null_encryptor ||= ActiveRecordEncryption::ReadOnlyNullEncryptor.new
+    def null_encryptor
+      @null_encryptor ||= ActiveRecordEncryption::NullEncryptor.new
     end
 end
