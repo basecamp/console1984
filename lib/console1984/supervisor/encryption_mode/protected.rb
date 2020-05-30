15048bc2f0029b1a085ba48c4fa65661e1c46276
Jorge Manrubia <jorge.manrubia@gmail.com>
2020-05-30 10:08:47 +0200

    Use the new method to protect execution of code with encryption

--- lib/console1984/supervisor/encryption_mode/protected.rb
+++ lib/console1984/supervisor/encryption_mode/protected.rb
@@ -1,6 +1,6 @@
 class Console1984::Supervisor::EncryptionMode::Protected
   def execute(&block)
-    ActiveRecordEncryption.with_encryption_context(encryptor: null_encryptor, frozen_encryption: true, &block)
+    ActiveRecordEncryption.protecting_encrypted_data(&block)
   end
 
   private
