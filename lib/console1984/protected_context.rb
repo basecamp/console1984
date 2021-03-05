ae571ede905fca4ee08930be048daffbe61e10b8
Jorge Manrubia <jorge.manrubia@gmail.com>
2021-03-05 15:54:55 +0100

    Protect values evaluated in the console too

--- lib/console1984/protected_context.rb
+++ lib/console1984/protected_context.rb
@@ -1,6 +1,12 @@
 module Console1984::ProtectedContext
   # Protect the code to show inspected objects too. This method is invoked
   # for showing returned objects in the console
+  def inspect_last_value
+    Console1984.supervisor.execute do
+      super
+    end
+  end
+
   def evaluate(line, line_no, exception: nil)
     Console1984.supervisor.execute do
       super
