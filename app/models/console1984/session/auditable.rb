b719596b8851fce8fbd3960cdd557f05d3e58a76
Jorge Manrubia <jorge.manrubia@gmail.com>
2021-08-13 10:41:01 +0200

    Show only sessions flagged

--- app/models/console1984/session/auditable.rb
+++ app/models/console1984/session/auditable.rb
@@ -6,6 +6,8 @@ module Console1984::Session::Auditable
 
     scope :sensitive, -> { joins(:sensitive_accesses) }
     scope :reviewed, -> { joins(:audits) }
+    scope :approved, -> { reviewed.where("audits.status": :approved) }
+    scope :flagged, -> { reviewed.where("audits.status": :flagged) }
     scope :pending, -> { where.not(id: reviewed) }
   end
 end
