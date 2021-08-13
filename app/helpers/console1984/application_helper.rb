68a49ca1c01f5e77150b8f7f03e3d8996f6cca37
Jorge Manrubia <jorge.manrubia@gmail.com>
2021-08-13 06:54:15 +0200

    Improve how session headers look

--- app/helpers/console1984/application_helper.rb
+++ app/helpers/console1984/application_helper.rb
@@ -12,6 +12,10 @@ module Console1984
       date.strftime("%Y-%m-%d at %I:%m %P")
     end
 
+    def highlighted_code_from(commands)
+      highlight_code commands.collect(&:statements).collect(&:strip).filter(&:present?).join("\n")
+    end
+
     def highlight_code(source)
       formatter = Rouge::Formatters::HTMLLinewise.new(Rouge::Formatters::HTML.new)
       lexer = Rouge::Lexers::Ruby.new
