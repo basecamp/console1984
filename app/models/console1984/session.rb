1e444cbf212399f8671207aa27cb4102caec95fd
Jorge Manrubia <jorge.manrubia@gmail.com>
2021-08-13 06:43:04 +0200

    Render code grouped by sensitive accesses

--- app/models/console1984/session.rb
+++ app/models/console1984/session.rb
@@ -10,8 +10,24 @@ module Console1984
       sensitive_accesses.any?
     end
 
-    def executed_code
-      commands.sorted_chronologically.collect(&:statements).collect(&:strip).join("\n")
+    # Loops through all the session commands in order, yielding lists grouped by
+    # its sensitive access record, or its absence
+    def each_batch_of_commands_grouped_by_sensitive_access
+      group = []
+      current_sensitive_access = nil
+      commands.includes(:sensitive_access).sorted_chronologically.each.with_index do |command, index|
+        group << command
+
+        if index > 0 && command.sensitive_access != current_sensitive_access
+          yield current_sensitive_access, group
+          group = []
+          current_sensitive_access = command.sensitive_access
+        end
+      end
+
+      if group.present?
+        yield current_sensitive_access, group
+      end
     end
   end
 end
