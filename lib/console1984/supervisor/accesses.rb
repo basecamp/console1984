7bb0d97690dcf3c90433ad5bc7835ffbcc59f0de
Jorge Manrubia <jorge.manrubia@gmail.com>
2021-08-17 11:13:28 +0200

    Don't return the sensitive session record when decrypting sessions

--- lib/console1984/supervisor/accesses.rb
+++ lib/console1984/supervisor/accesses.rb
@@ -6,6 +6,7 @@ module Console1984::Supervisor::Accesses
       show_warning Console1984.enter_unprotected_encryption_mode_warning if !silent && protected_mode?
       justification = ask_for_value "\nBefore you can access personal information, you need to ask for and get explicit consent from the user(s). #{current_username}, where can we find this consent (a URL would be great)?"
       session_logger.start_sensitive_access justification
+      nil
     end
   ensure
     @access = Unprotected.new
@@ -16,6 +17,7 @@ module Console1984::Supervisor::Accesses
     run_system_command do
       show_warning Console1984.enter_protected_mode_warning if !silent && unprotected_mode?
       session_logger.end_sensitive_access
+      nil
     end
   ensure
     @access = Protected.new
