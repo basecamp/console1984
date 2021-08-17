5d6a14a944e112e2fa32066cbb75dc9f00a72e56
Jorge Manrubia <jorge.manrubia@gmail.com>
2021-08-17 12:20:04 +0200

    Silence Active record and Active job when persisting auditing trails

--- lib/console1984/sessions_logger/database.rb
+++ lib/console1984/sessions_logger/database.rb
@@ -2,8 +2,10 @@ class Console1984::SessionsLogger::Database
   attr_reader :current_session, :current_sensitive_access
 
   def start_session(username, reason)
-    user = Console1984::User.create_or_find_by!(username: username)
-    @current_session = user.sessions.create! reason: reason
+    silence_logging do
+      user = Console1984::User.create_or_find_by!(username: username)
+      @current_session = user.sessions.create! reason: reason
+    end
   end
 
   def finish_session
@@ -12,7 +14,9 @@ class Console1984::SessionsLogger::Database
   end
 
   def start_sensitive_access(justification)
-    @current_sensitive_access = current_session.sensitive_accesses.create! justification: justification
+    silence_logging do
+      @current_sensitive_access = current_session.sensitive_accesses.create! justification: justification
+    end
   end
 
   def end_sensitive_access
@@ -20,20 +24,34 @@ class Console1984::SessionsLogger::Database
   end
 
   def before_executing(statements)
-    @before_commands_count = @current_session.commands.count
-    record_statements statements
+    silence_logging do
+      @before_commands_count = @current_session.commands.count
+      record_statements statements
+    end
   end
 
   def after_executing(statements)
   end
 
   def suspicious_commands_attempted(statements)
-    sensitive_access = start_sensitive_access "Suspicious commands attempted"
-    Console1984::Command.last.update! sensitive_access: sensitive_access
+    silence_logging do
+      sensitive_access = start_sensitive_access "Suspicious commands attempted"
+      Console1984::Command.last.update! sensitive_access: sensitive_access
+    end
   end
 
   private
     def record_statements(statements)
       @current_session.commands.create! statements: Array(statements).join("\n"), sensitive_access: current_sensitive_access
+    end
+
+    def silence_logging(&block)
+      if Console1984.debug
+        block.call
+      else
+        Console1984::IncinerationJob.logger.silence do
+          Console1984::Base.logger.silence(&block)
+        end
+      end
     end
 end
