744b9a91fbdccde2dcebe5d4d2dbd32c139cf752
Lewis Buckley <lewis@basecamp.com>
2021-11-01 13:18:37 +0000

    Prefer double quoted strings

--- test/ext/socket/tcp_socket_test.rb
+++ test/ext/socket/tcp_socket_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class TCPSocketTest < ActiveSupport::TestCase
   test "doesn't raise when forwarding kwargs" do
     assert_nothing_raised do
-      socket = TCPSocket.new 'localhost', 6379
+      socket = TCPSocket.new("localhost", 6379)
       socket.write_nonblock "forbidden request!", exception: false
     end
   end
