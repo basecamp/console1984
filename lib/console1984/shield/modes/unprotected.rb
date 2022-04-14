b048f8c5cb34c16432e621a9e6e0a58c5a651996
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-14 15:17:07 +0800

    Delete lib directory

--- lib/console1984/shield/modes/unprotected.rb
+++
@@ -1,8 +0,0 @@
-# An execution mode that doesn't protect encrypted information or external systems.
-class Console1984::Shield::Modes::Unprotected
-  include Console1984::Freezeable
-
-  def execute(&block)
-    block.call
-  end
-end
--- lib/console1984/version.rb
+++
@@ -1,3 +0,0 @@
-module Console1984
-  VERSION = '0.1.24'
-end
