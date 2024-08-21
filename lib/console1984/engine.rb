2d4d673c26753adda9015cee91b850b460d15ca6
Alex Ghiculescu <alex@tanda.co>
2024-08-22 09:14:43 +1000

    Update lib/console1984/engine.rb
    Co-authored-by: Neil Carvalho <me@neil.pro>

--- lib/console1984/engine.rb
+++ lib/console1984/engine.rb
@@ -8,7 +8,7 @@ module Console1984
     config.console1984.protected_environments ||= %i[ production ]
     config.console1984.protected_urls ||= []
 
-    # Console 1984 doesn't work with the sandboxed Rails console, becuase
+    # Console 1984 doesn't work with the sandboxed Rails console, because
     # it can't write its audit entries to the database.
     initializer "console1984.disable_sandbox" do |app|
       app.config.disable_sandbox = true
